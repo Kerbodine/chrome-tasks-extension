280a904c5b03b58476f6e1edc542ee8966445765
Michael Tong <michaelyuyangtong@gmail.com>
2022-03-14 18:46:45 +0800

    Added css bugfixes

--- src/App.js
+++ src/App.js
@@ -7,18 +7,26 @@ import { useSettings } from "./contexts/SettingsContext";
 const App = () => {
   const { settings, toggleTheme } = useSettings();
 
+  // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+  if (
+    settings.darkMode &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  ) {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
+
   return (
-    <div className={`${settings.darkMode && "dark"}`}>
-      <div className="relative w-full h-screen flex flex-col items-center justify-center gap-8 text-base dark:bg-gray-900 transition-colors">
-        <TimeDisplay />
-        <TaskList />
-        <button
-          onClick={toggleTheme}
-          className="absolute right-4 top-4 text-2xl bg-gray-100 rounded-full p-2 text-black dark:bg-gray-800 dark:text-white"
-        >
-          {settings.darkMode ? <BiSun /> : <BiMoon />}
-        </button>
-      </div>
+    <div className="relative w-full h-screen flex flex-col items-center justify-center gap-8 text-base transition-colors">
+      <TimeDisplay />
+      <TaskList />
+      <button
+        onClick={toggleTheme}
+        className="absolute right-4 top-4 text-2xl bg-gray-100 rounded-full p-2 text-black dark:bg-gray-800 dark:text-white"
+      >
+        {settings.darkMode ? <BiSun /> : <BiMoon />}
+      </button>
     </div>
   );
 };
